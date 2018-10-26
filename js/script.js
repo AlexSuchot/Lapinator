a94f5b8e4ad2b01ff53151cab46180b57a88de93
AlexSuchot <33685833+AlexSuchot@users.noreply.github.com>
2018-10-26 15:33:09 +0200

    Adding project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function burgerMenu() {
+    var element = document.getElementById("myTopNav");
+    element.classList.toggle("responsiveNavbar");
+    element.classList.toggle("fadeInDown");
+    element.classList.toggle("animated");
+
+
+}
